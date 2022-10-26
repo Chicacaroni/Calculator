4187bce142e703433d101942a1bbf53ed7157d98
unknown <dvlaxamana.student@ua.edu.ph>
2022-10-26 09:26:58 +0800

    third commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+
 function CalcDisplay({display}) {
   return (
     <div className="calcDisplay">
@@ -16,35 +17,106 @@ function CalcButton({value, onClick}) {
   );
 }
 
+function NegateButton({value,onClick}){
+  return (
+    <button className="calcButton" onClick={onClick}>
+      negate
+    </button>
+  )
+}
+
+function ClearButton({value, onClick}){
+  return (
+    <button className="calcButton redcolor" onClick={onClick}>
+      {value}
+    </button>
+  );
+}
+
 function App() {
-  
+
   const [num1, setNum1] = useState(0);
   const [num2, setNum2] = useState(0);
   const [oper, setOper] = useState(0);
   const [res, setRes] = useState(0);
   const [disp, setDisp] = useState(0);
+  const[nega, setNega] = useState(0);
 
   const numberClickHandler = (e) => {
     e.preventDefault();
     const value = e.target.innerHTML;
- 
+
     if (oper === 0) {
       if (parseInt(num1) === 0) {
         setNum1(value)
         setDisp(value);
-      } else {
-        setNum1(num1+value)
-        setDisp(num1+value);
       }
-    } else {
-      if (num2 === 0) {
-        setNum2(value)
-        setDisp(value);      
-    } else {
-        setNum2(num2+value)
-        setDisp(num2+value);
+      else{
+        if(String(num1).length<10){
+          setNum1(num1+value)
+          setDisp(num1+value);
+        }
       }
     }
+    else if(oper=="negate"){
+      if (parseInt(num1) === 0) {
+        setNum1("-"+value)
+        setDisp("-"+value);
+      }
+      else{
+        if(String(num1).length<10){
+          setNum1(num1+value)
+          setDisp(num1+value);
+        }
+      }
+    }
+    else if(oper!="negate"){
+      if (parseInt(num2) === 0) {
+        setNum2(value)
+        setDisp(value);
+      }
+      else{
+        if(String(num2).length<10){
+          setNum2(num2+value)
+          setDisp(num2+value);
+        }
+      }
+    }
+    else{
+      setDisp("Error negate only once at the beginning");
+    }
+    console.log(num1 +'|'+ num2 +'|'+ oper +'|'+ res +'|'+ disp);
+  };
+  
+  const onOffClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+    if(value === "ON"){
+      e.target.innerHTML = "OFF";
+      setDisp("");
+      CalcButton.disabled = true;
+    }
+    else if(value === "OFF"){
+      e.target.innerHTML = "ON";
+      setDisp(0);
+    }
+    }
+
+    
+  
+
+
+
+
+  const deleteClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+    if(oper === 0){
+      setNum1(num1.slice(0, -1));
+      setDisp(disp.slice(0, -1));
+    };
+
+
     console.log(num1 +'|'+ num2 +'|'+ oper +'|'+ res +'|'+ disp);
   };
 
@@ -56,32 +128,51 @@ function App() {
 
     console.log(num1 +'|'+ num2 +'|'+ oper +'|'+ res +'|'+ disp);
   };
+  
 
   const equalClickHandler = () => {
     console.log(num1 +'|'+ num2 +'|'+ oper +'|'+ res +'|'+ disp);
-    
-    if(oper === "+") {
+
+    if(oper === "+")
+    {
       setRes(parseInt(num1) + parseInt(num2));
       setDisp(parseInt(num1) + parseInt(num2));
-    } else if (oper === "-") {
+    }
+
+    else if (oper === "-")
+    {
       setRes(parseInt(num1) - parseInt(num2));
-      setDisp(parseInt(num1) - parseInt(num2));      
-    } else if (oper === "*") {
+      setDisp(parseInt(num1) - parseInt(num2));
+
+    } else if(oper === "*")
+    {
       setRes(parseInt(num1) * parseInt(num2));
-      setDisp(parseInt(num1) * parseInt(num2)); 
-    } else if (oper === "/") {
+      setDisp(parseInt(num1) * parseInt(num2));
+    } else if(oper === "/")
+    {
       setRes(parseInt(num1) / parseInt(num2));
-      setDisp(parseInt(num1) / parseInt(num2)); 
-    } else {
+      setDisp(parseInt(num1) / parseInt(num2));
+    }else if(oper === "^")
+    {
+      setRes(parseInt(num1) ** parseInt(num2));
+      setDisp(parseInt(num1) ** parseInt(num2));
+    }
+    else if(oper === "%")
+    {
+      setRes(parseInt(num1) % parseInt(num2));
+      setDisp(parseInt(num1) % parseInt(num2));
+    }
+    else {
       setDisp("ERROR");
       setNum1(0);
       setNum2(0);
       setOper(0);
       setRes(0);
-      setDisp(0);            
+      setDisp(0);
     }
   };
 
+  /*This is now working!*/
   const clearClickHandler = () => {
     setNum1(0);
     setNum2(0);
@@ -92,7 +183,7 @@ function App() {
     console.log(num1 +'|'+ num2 +'|'+ oper +'|'+ res +'|'+ disp);
   };
 
-  return ( 
+  return (
     <div className="calcContainer">
       <CalcDisplay display={disp}/>
       <div className="calcButtonsContainer">
@@ -108,12 +199,18 @@ function App() {
         <CalcButton value="2" onClick={numberClickHandler}/>
         <CalcButton value="1" onClick={numberClickHandler}/>
         <CalcButton value="*" onClick={operClickHandler}/>
-        <CalcButton value="C" onClick={clearClickHandler}/>
+        <ClearButton value="C" onClick={clearClickHandler}/>
         <CalcButton value="0" onClick={numberClickHandler}/>
         <CalcButton value="=" onClick={equalClickHandler}/>
         <CalcButton value="/" onClick={operClickHandler}/>
-      </div>  
-    </div>  
+        <NegateButton value="negate" onClick={operClickHandler}/>
+        <CalcButton value="Del" onClick={deleteClickHandler}/>
+        <CalcButton value="ON" onClick={onOffClickHandler}/>
+        <CalcButton value="^" onClick={operClickHandler}/>
+        <CalcButton value="%" onClick={operClickHandler}/>
+
+      </div>
+    </div>
   );
 }
 
